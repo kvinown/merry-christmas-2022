2a8e16eb2096e381b96679df76feef4990bd1649
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2022-12-24 21:59:41 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== PARALLAX ===============*/
+let parallax = new Rellax('.parallax');
+
+/*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__village', 1.2, {opacity: 0, y: 100, delay: .1})
+gsap.from('.home__pine', 1.2, {opacity: 0, y: 150, delay: .3})
+gsap.from('.home__mountain-2', 1.2, {opacity: 0, x: 150, delay: .5})
+gsap.from('.home__mountain-3', 1.2, {opacity: 0, x: -150, delay: .6})
+gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, delay: .7})
+gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, delay: .8})
+gsap.from('.home__trieno', 1.2, {opacity: 0, x: -200, delay: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, y: -60, delay: 1})
